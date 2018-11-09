dbf509e6567a4dc791a57a4e921c62d6015110f8
Jon Gray <jgray@spartaglobal.com>
2018-11-09 10:59:47 +0000

    dummy, precompiled assets

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :duelists
   root to: "duelists#index"
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrailasdasdasds.org/routing.html
 end
