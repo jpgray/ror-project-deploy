e7eed814a7402f4f48a92eb565fccf0917d35edd
Jon Gray <jgray@spartaglobal.com>
2018-11-09 15:55:52 +0000

    v.0.95 minor miscode fix for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {}).permit(:email, :username, :password, :created_at, :updated_at:)
+      params.fetch(:user, {})
     end
 end
