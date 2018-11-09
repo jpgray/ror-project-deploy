3b3093eb92acc44bb621fa79862324ca9ec8b284
Jon Gray <jgray@spartaglobal.com>
2018-11-09 15:52:31 +0000

    v.0.9 full buttons update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_field, :username)
+      params.fetch(:user, {}).permit(:email, :username, :password, :created_at, :updated_at:)
     end
 end
